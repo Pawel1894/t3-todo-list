998277074e3a3dc32ac975540de2c1a891dfbf2b
Pawel1894 <pawepohl@gmail.com>
2023-03-08 18:43:37 +0100

    Adding load indicator when awaiting for session data

---
+++ src/components/LoadIndicator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function LoadIndicator() {
+  return (
+    <div className="h-12 w-12 animate-spin rounded-full border border-primary-100 border-t-[transparent]"></div>
+  );
+}
