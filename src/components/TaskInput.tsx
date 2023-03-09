cf7215db16244ffbe0568d3267da7d6d409aadf7
Pawel1894 <pawepohl@gmail.com>
2023-03-09 20:34:11 +0100

    dark mode for task input

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { api } from "~/utils/api";
 
 export default function TaskInput() {
@@ -18,19 +17,35 @@ export default function TaskInput() {
         value={task}
         onChange={(e) => setTask(e.target.value)}
         type="text"
-        className="w-full rounded-l-md py-[0.875rem] px-5 outline-none"
+        className="w-full rounded-l-md py-[0.875rem] px-5 outline-none dark:bg-dark-200 dark:text-white"
         placeholder="Create a new todo..."
       />
       <button
-        className="rounded-r-md bg-white px-5 focus:bg-light-200 active:bg-light-200"
+        className="bg- rounded-r-md bg-light-200 px-5 focus:bg-white active:bg-white dark:bg-dark-100 dark:text-white dark:focus:bg-dark-100 dark:active:bg-dark-100"
         onClick={createTask}
       >
-        <Image
-          width={20}
-          height={20}
-          src="/images/icon-save.svg"
-          alt="save task"
-        />
+        <svg
+          className="fill-dark-200 dark:fill-white "
+          version="1.1"
+          id="Capa_1"
+          width="20px"
+          height="20px"
+          viewBox="0 0 407.096 407.096"
+        >
+          <g>
+            <g>
+              <path
+                d="M402.115,84.008L323.088,4.981C319.899,1.792,315.574,0,311.063,0H17.005C7.613,0,0,7.614,0,17.005v373.086
+			c0,9.392,7.613,17.005,17.005,17.005h373.086c9.392,0,17.005-7.613,17.005-17.005V96.032
+			C407.096,91.523,405.305,87.197,402.115,84.008z M300.664,163.567H67.129V38.862h233.535V163.567z"
+              />
+              <path
+                d="M214.051,148.16h43.08c3.131,0,5.668-2.538,5.668-5.669V59.584c0-3.13-2.537-5.668-5.668-5.668h-43.08
+			c-3.131,0-5.668,2.538-5.668,5.668v82.907C208.383,145.622,210.92,148.16,214.051,148.16z"
+              />
+            </g>
+          </g>
+        </svg>
       </button>
     </div>
   );
